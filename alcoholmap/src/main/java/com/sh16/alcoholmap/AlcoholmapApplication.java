e2d79cf3ea96fa39a6a3fcaeaefa074bd1d0889c
birdzero <redviking2@naver.com>
2024-02-05 19:42:16 +0900

    Add - spring project

---
+++ alcoholmap/src/main/java/com/sh16/alcoholmap/AlcoholmapApplication.java
@@ -0,0 +1,13 @@
+package com.sh16.alcoholmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcoholmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcoholmapApplication.class, args);
+	}
+
+}
