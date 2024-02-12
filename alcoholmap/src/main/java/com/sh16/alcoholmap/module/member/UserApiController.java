97959bb82199c932bcba33e9a1a8a17e15c8aaa6
birdzero <redviking2@naver.com>
2024-02-12 22:59:48 +0900

    Feat - logout 구현

--- alcoholmap/src/main/java/com/sh16/alcoholmap/module/member/UserApiController.java
+++ alcoholmap/src/main/java/com/sh16/alcoholmap/module/member/UserApiController.java
@@ -1,7 +1,12 @@
 package com.sh16.alcoholmap.module.member;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,6 +21,13 @@ public class UserApiController {
     public String signup(UserDTO dto) {
         userService.save(dto);
         return "redirect:/login"; //회원가입 후 /login URL로 이동
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 
 
