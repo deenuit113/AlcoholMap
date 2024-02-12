67767b9645d307344bb76597da45c715fccff63e
birdzero <redviking2@naver.com>
2024-02-12 20:04:01 +0900

    Feat - User 클래스, 레포지토리 구현

--- alcoholmap/src/main/java/com/sh16/alcoholmap/common/config/SecurityConfiguration.java
+++ alcoholmap/src/main/java/com/sh16/alcoholmap/common/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.sh16.alcoholmap.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
--- alcoholmap/src/main/java/com/sh16/alcoholmap/module/member/MemberController.java
+++ alcoholmap/src/main/java/com/sh16/alcoholmap/module/member/MemberController.java
@@ -1,14 +1,14 @@
-package com.sh16.alcoholmap.module.member.controller;
+package com.sh16.alcoholmap.module.member;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user/**")
+@RequestMapping("/user")
 public class MemberController {
 
     //회원가입 페이지 이동
-    @RequestMapping("signup")
+
     public String create(){
         return "user/signup";
     }
