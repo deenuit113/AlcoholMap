ae71143de516df4f12b917ce918873d9d6cafd6f
birdzero <redviking2@naver.com>
2024-02-12 21:50:13 +0900

    Feat - UserDTO 생성

---
+++ alcoholmap/src/main/java/com/sh16/alcoholmap/module/member/UserDTO.java
@@ -0,0 +1,13 @@
+package com.sh16.alcoholmap.module.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private String email;
+    private String password;
+
+
+}
