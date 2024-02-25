3878dcc7dd84cfa5e78ef48740feef4cb366c350
deenuit113 <leejh5314@gmail.com>
2024-02-25 15:27:55 +0900

    Feat: Add nav bar and buttons 내비게이션 바와 다른 페이지로 이등할 버튼 추가

--- alcoholmap/src/frontend/pages/map/index.js
+++ alcoholmap/src/frontend/pages/map/index.js
@@ -1,9 +1,12 @@
-import {Greeting, Map} from '../../styles/mapStyle'
+import { Wrapper, AMTitle, AMHeader, MapMain, MypageButton, MapNav, LoginButton, SignupButton} from '../../styles/mapStyle'
 import { useEffect, useState } from 'react'
 
+import { useRouter } from 'next/router'
 
 export default function MapPage() {
     
+    const router = useRouter()
+
     useEffect(() => {
         const container = document.getElementById('map'); // 지도를 담을 영역의 DOM 레퍼런스
         const options = { // 지도를 생성할 때 필요한 기본 옵션
@@ -14,14 +17,42 @@ export default function MapPage() {
         const map = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
     }, []);
 
+    
+
+    // 온클릭 이벤트
+    const onClickMypage = () => {
+        router.push("../mypage")
+    }
+
+    const onClickLogin = () => {
+        router.push("../login")
+    }
+
+    const onClickSignup = () => {
+        router.push("../signup")
+    }
+
+    const onClickReload = () => {
+        window.location.reload();
+    }
+
     return (
         <>
             <script 
                 type="text/javascript" 
                 src="//dapi.kakao.com/v2/maps/sdk.js?appkey=874eea7b48b7810e4c254737d3892e8f"
             ></script>
-            <Greeting>Greeting Message</Greeting>
-            <Map id="map"></Map>
+            <Wrapper>
+                <AMHeader>
+                    <AMTitle onClick={onClickReload}>AlcoholMap</AMTitle>
+                </AMHeader>
+                <MapNav>
+                    <LoginButton onClick={onClickLogin}>로그인</LoginButton>
+                    <SignupButton onClick={onClickSignup}>회원가입</SignupButton>
+                    <MypageButton onClick={onClickMypage}>마이페이지</MypageButton>
+                </MapNav>
+                <MapMain id="map"></MapMain>
+            </Wrapper>
         </>
         
 
