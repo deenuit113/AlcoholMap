33b62cfb7db0bcdeb3eb5a496dbf78c035dda4d0
birdzero <redviking2@naver.com>
2024-03-03 23:51:42 +0900

    Feat: Place 엔티티 작성(술집)

---
+++ alcoholmap/src/main/java/com/sh16/alcoholmap/module/place/Place.java
@@ -0,0 +1,35 @@
+package com.sh16.alcoholmap.module.place;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Place {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    private double latitude;
+
+    private double longitude;
+
+    private String phone;
+
+    private String location;
+
+    private String operationHours;
+
+    private int categoryId;
+}
---
+++ alcoholmap/src/main/java/com/sh16/alcoholmap/module/review/ReviewController.java
@@ -0,0 +1,10 @@
+package com.sh16.alcoholmap.module.review;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReviewController {
+}
