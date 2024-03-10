7103397fcbf860507757146f6ad7832bfddaae2c
deenuit113 <leejh5314@gmail.com>
2024-03-11 00:53:01 +0900

    Refactor: migrate JS to TS Javascript에서 Typescript로 마이그레이션

--- alcoholmap/src/frontend/pages/index.tsx
+++ alcoholmap/src/frontend/pages/index.tsx
@@ -1,14 +1,12 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>index page</title>
-      </Head>
       <main className={styles.main}>
-          <p>pages/index.js</p>
+          <p>
+            안녕하세요, 알콜맵입니다.
+          </p>
       </main>
     </>
   )
