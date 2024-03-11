46314674bd258b92cf2ecc1c6f69bf7f9c02365d
birdzero <redviking2@naver.com>
2024-03-11 23:03:52 +0900

    Refactor: PK 컬럼명 수정

--- alcoholmap/src/main/java/com/sh16/alcoholmap/module/place/Place.java
+++ alcoholmap/src/main/java/com/sh16/alcoholmap/module/place/Place.java
@@ -1,9 +1,6 @@
 package com.sh16.alcoholmap.module.place;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class Place {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "place_id")
     private int id;
 
     private String name;
