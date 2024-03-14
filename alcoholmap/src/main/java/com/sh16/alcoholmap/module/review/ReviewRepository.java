3aeab510b9d5388b5913848410bf7509d6aae1b1
birdzero <redviking2@naver.com>
2024-03-14 23:59:30 +0900

    Feat: 식당 별 리뷰 조회 기능 구현

--- alcoholmap/src/main/java/com/sh16/alcoholmap/module/review/ReviewRepository.java
+++ alcoholmap/src/main/java/com/sh16/alcoholmap/module/review/ReviewRepository.java
@@ -1,6 +1,18 @@
 package com.sh16.alcoholmap.module.review;
 
+import com.sh16.alcoholmap.module.place.Place;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    Optional<Place> findPlacesById(Long placeId);
+
+    int getPlaceReviewsNumByPlaceId(Long placeId);
+
+    Optional<Float> getPlaceReviewsAVGByPlaceId(Long placeId);
+
+    List<Object[]> getPlaceReviewsByPlaceId(Long placeId, PageRequest pageRequest);
 }
--- alcoholmap/src/main/java/com/sh16/alcoholmap/module/review/ReviewService.java
+++ alcoholmap/src/main/java/com/sh16/alcoholmap/module/review/ReviewService.java
@@ -3,12 +3,15 @@ package com.sh16.alcoholmap.module.review;
 import com.sh16.alcoholmap.module.member.Response;
 import com.sh16.alcoholmap.module.member.User;
 import com.sh16.alcoholmap.module.member.UserRepository;
+import com.sh16.alcoholmap.module.place.Place;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -38,4 +41,53 @@ public class ReviewService {
         return Response.newResult(HttpStatus.OK, "리뷰 추가에 성공하였습니다.", null);
     }
 
+
+    /**
+     * 리뷰 조회 (술집 별 조회)
+     * @param placeId
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    public ResponseEntity<Response> getPlaceAllReviewsByPlaceId(Long placeId, int page, int pageSize) {
+        Optional<Place> place = reviewRepository.findPlacesById(placeId);
+        if (!place.isPresent()) {
+            return Response.newResult(HttpStatus.BAD_REQUEST, placeId + "는 존재하지 않는 맛집입니다.", null);
+        }
+        int totalPageItemCnt = reviewRepository.getPlaceReviewsNumByPlaceId(placeId);
+
+        HashMap<String, Object> hashMap = new LinkedHashMap<>();
+        hashMap.put("totalPageItemCnt", totalPageItemCnt);
+        hashMap.put("totalPage", ((totalPageItemCnt - 1) / pageSize) + 1);
+        hashMap.put("nowPage", page);
+        hashMap.put("nowPageSize", pageSize);
+
+        Optional<Float> totalStarRate = reviewRepository.getPlaceReviewsAVGByPlaceId(placeId);
+        if (totalStarRate.isPresent()) {
+            hashMap.put("totalStarRate", totalStarRate.get());
+        } else {
+            hashMap.put("totalStarRate", null);
+        }
+
+        PageRequest pageRequest = PageRequest.of(page - 1, pageSize);
+        List<Object[]> items = reviewRepository.getPlaceReviewsByPlaceId(placeId, pageRequest);
+        ArrayList<PlaceReviewAndUserResponse> arr = new ArrayList<>();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+
+        for (Object[] item : items) {
+            String format = formatter.format(item[4]);
+            arr.add(PlaceReviewAndUserResponse.builder()
+                    .reviewId((int) item[0])
+                    .starRate((float) item[1])
+                    .content((String) item[2])
+                    .userId((String) item[3])
+                    .craeteDate(format)
+                    .reviewImage((String) item[5])
+                    .userNickname((String) item[6])
+                    .userProfileImage((String) item[7])
+                    .build());
+        }
+        hashMap.put("reviews", arr);
+        return Response.newResult(HttpStatus.OK, placeId + "의 모든 리뷰 목록 조회에 성공했습니다.", hashMap);
+    }
 }
